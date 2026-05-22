4325a43ec8d32b19441b66cb8cc0d276523ad1e3
tony2026-1 <tonybryham1234@gmail.com>
2026-05-22 17:56:05 -0300

    update

---
+++ atividades/1atividade.py
@@ -0,0 +1,13 @@
+"""
+1 - crie um cadastro pessoal com:
+nome
+idade
+altura
+depois, mostre na tela a conclusão do cadastro em uma mensagem formatada
+2 - faça a notinha da compra da pessoa com:
+nome do produto
+quantidade
+preço
+depois, mostre na tela as informações de pagamento em uma mensagem formatada
+3 - faça uma calculadora que faça soma entre 2 valores, depois exiba em uma mensagem formatada
+"""
